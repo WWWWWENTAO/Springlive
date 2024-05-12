089b665258748150e4abd757fc056e609e131f55
Wentao Huang <hwt990706@gmail.com>
2024-05-12 13:49:04 -0800

    setup OrderService with Spring Web / Spring Data JPA / MySQL Driver / Cloud Bootstrap / Eureka Discovery Client / Lombok

---
+++ OrderSeervice/src/main/java/com/example/OrderSeervice/OrderSeerviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrderSeervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSeerviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSeerviceApplication.class, args);
+	}
+
+}
---
+++ OrderSeervice/src/test/java/com/example/OrderSeervice/OrderSeerviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrderSeervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSeerviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

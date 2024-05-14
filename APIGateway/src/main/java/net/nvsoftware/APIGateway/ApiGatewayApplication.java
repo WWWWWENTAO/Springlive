d5ec2422f401e06e52206cf8d67942b4d2afded5
Wentao Huang <hwt990706@gmail.com>
2024-05-14 15:10:12 -0800

    Init API Gateway to access the server in different local port as a way to do Replica

--- APIGateway/src/main/java/net/nvsoftware/APIGateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/net/nvsoftware/APIGateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package net.nvsoftware.APIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.keyvalue.core.mapping.KeySpaceResolver;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
